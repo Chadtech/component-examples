9b7fb012d38b83e6a7ba79dbb2f7287969144aae
Chad Stearns <chad@superfocus.ai>
2025-03-31 03:11:45 -0400

    Title, and a link to the repo

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,5 @@
 module Main exposing (borderColor, main)
 
-{-| This repo is meant to demonstrate how I would make various components in Elm.
-
-Making components is tricky. The fundamental reason to make re-usable components,
-is to unite various implementations under one common code source. In this way,
-changes made to the component are reflected at all the places it is used. I think
-this is counter-intuitive to many people, who think that if two things are the same
-(two buttons, for example), they should be the same code. This is true only to the
-extent that we expect the buttons to change together. If two buttons are identical,
-but we know that one button will evolve over time in ways that will interfere with
-the implementation details of the other, they should not be based on a re-usable
-component. Conversely, if two things are not the same, but maybe share one important
-attribute, such as their width, they _should_ be based on re-usable code. That way,
-if we need to change the width, all things that share that width will change
-together, regardless as to whether those things are in any sense "the same".
-
-Furthermore, this question of "do these things change together" applies not only to
-the decision of whether to make something a re-usable component, but which parts
-should be re-usable. Modals, for example, are all similar in their placement on the
-screen, their general layout, and their behavior of closing when clicked outside of.
-But beyond that, they are pretty different, and might have wildly different content
-in the body of the modal. Given these details, Modal components should generally
-re-use code for the container of the modal, but the body of the internal content of
-a modal should be pretty open and loosely defined with few requirements.
-
--}
-
 import Browser exposing (Document)
 import Css
 import Css.Global
@@ -238,9 +212,27 @@ globalStyles =
 
 view : Model -> List (Html Msg)
 view model =
-    globalStyles
-        :: intro
-        :: List.map (componentView model) allComponents
+    [ globalStyles
+    , Html.div
+        [ Attr.css
+            [ S.col ]
+        ]
+        [ Html.span
+            [ Attr.css
+                [ S.textXl ]
+            ]
+            [ Html.text "Bellroy Components Examples - Chad Stearns"
+            ]
+        , Html.a
+            [ Attr.href "https://github.com/Chadtech/component-examples"
+            , Attr.css
+                [ S.textXl ]
+            ]
+            [ Html.text "See the source Code here" ]
+        ]
+    , intro
+    ]
+        ++ List.map (componentView model) allComponents
 
 
 intro : Html Msg
@@ -248,7 +240,6 @@ intro =
     Html.div
         [ Attr.css
             [ S.row
-            , S.justifyCenter
             ]
         ]
         [ Html.div
